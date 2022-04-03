f8668241088f2e4a8a9b81eca6f1b03a358e6b7a
izumi0x01 <izumisoh.develop@gmail.com>
2022-04-04 01:02:44 +0900

    first_commit:表示画面の大枠だけ完成

---
+++ App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using wpf_weather_forecast_application.view;
+using wpf_weather_forecast_application.viewmodel;
+
+namespace wpf_weather_forecast_application
+{
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var w = new MainView();
+            var vm = new MainViewModel();
+
+            w.DataContext = vm;
+            w.Show();
+        }
+    }
+}
