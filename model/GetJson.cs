21f7ca4646bb853a85c9347168867fb3735b7bf5
izumi0x01 <izumisoh.develop@gmail.com>
2022-04-16 11:23:25 +0900

    newton.jsonのインスコ

---
+++ model/GetJson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace wpf_weather_forecast_application.model
+{
+    internal class GetJson
+    {
+    }
+}
